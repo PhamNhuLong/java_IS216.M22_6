10c2a9db1ea4a30f198d7d3b997d26859a80529e
honghai19521467 <92149284+honghai19521467@users.noreply.github.com>
2022-06-25 12:12:35 +0700

    fix_UI_Pts

--- QuanLyThuVien/src/quanlythuvien/TrangChuQL.java
+++ QuanLyThuVien/src/quanlythuvien/TrangChuQL.java
@@ -305,7 +305,7 @@ public JPanel childpanel;
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addComponent(pnMain, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
